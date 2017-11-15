fec96aab4af11ccf63ef90a39a31d32959d77b3b
myay <mikail.yayla@tu-dortmund.de>
2017-11-15 18:47:24 +0100

    2d DFT, but not tested yet

--- DFT_2D/dft_2d.c
+++ DFT_2D/dft_2d.c
@@ -8,8 +8,8 @@
  * For example with X = 4 and Y = 4, the input in form of (real,imaginary) can be specified as
  * (1,-1) (1,0) (1,0) (1,0) (1,0) (1,-1) (1,0) (1,0) (1,0) (1,0) (1,-1) (1,0) (1,0) (1,0) (1,0) (1,-1).
  */
-#define X 2 // number of rows
-#define Y 2 // and collumns
+#define X 4 // number of rows
+#define Y 4 // and collumns
 
 #ifndef M_PI
 #define M_PI 3.1416
@@ -23,20 +23,20 @@ void read_input (const char* file_name)
   FILE *numbers;
   numbers = fopen(file_name, "r");
 
-  if (numbers == NULL){
-      printf("Error reading file, try ./dft_2d /path/to/file\n");
-      exit (0);
+  if (numbers == NULL)
+  {
+    printf("Error reading file, try ./dft_2d /path/to/file\n");
+    exit (0);
   }
 
-  double real = 0.0;
-  double img = 0.0;
+  double real = 0.0, img = 0.0;
 
-  for (int i = 0; i < X; i++)
+  for (int x = 0; x < X; x++)
   {
-    for (int j = 0; j < Y; j++)
+    for (int y = 0; y < Y; y++)
     {
-        fscanf(numbers, "(%lf,%lf) ", &real, &img);
-        input[i][j] = real + img * I;
+      fscanf(numbers, "(%lf,%lf) ", &real, &img);
+      input[x][y] = real + img * I;
     }
   }
 
@@ -45,24 +45,42 @@ void read_input (const char* file_name)
 
 void print_array (double complex toprint[][Y])
 {
-  for (int i = 0; i < X; i++)
+  for (int x = 0; x < X; x++)
   {
-    for (int j = 0; j < Y; j++)
+    for (int y = 0; y < Y; y++)
     {
-        printf( " %.3f+%.3fi\t\t", creal( toprint[i][j] ), cimag( toprint[i][j] ) );
+      printf( " %.3f+%.3fi\t\t", creal( toprint[x][y] ), cimag( toprint[x][y] ) );
     }
     printf("\n");
   }
   printf("\n");
 }
 
-
-void dft (double complex in[][Y], double complex out[][Y], bool inverse)
+void dft_2d (double complex in[][Y], double complex out[][Y], bool inverse)
 {
+  int u = 0, v = 0, x = 0, y = 0;
+  double cos_arg, sin_arg;
+  double complex outer_exp;
 
-
-
-
+  for (; u < X ; u++)
+  {
+    for (; v < Y ; v++)
+    {
+      for (; x < X; x++)
+      {
+        for (; y < Y; y++)
+          {
+              cos_arg = ((2*M_PI*v*y) / Y);
+              sin_arg = ((-1)*(2*M_PI*v*y) / Y);
+              out[u][v] += (in[x][y]) * (cos( cos_arg ) + sin( sin_arg )*I);
+          }
+          cos_arg = ((2*M_PI*u*x) / X);
+          sin_arg = ((-1)*(2*M_PI*u*x) / X);
+          outer_exp = (cos( cos_arg ) + sin( sin_arg ) * I);
+          out[u][v] *= outer_exp;
+      }
+    }
+  }
   /*
   double complex *in_ptr = in;
   double complex *out_ptr = out;
@@ -106,12 +124,13 @@ int main (int argc, char *argv[])
   }
 
   read_input(argv[1]);
+
+
+  //dft(input, output, 0);
+
+  dft_2d(input, output, 1);
   print_array(input);
   print_array(output);
 
-  //dft(input, output, 0);
-  /*
-  dft(output, input, 1);
-  */
   return 0;
 }
