8ffedc6a7ced2043edf0ce8254420c1d3202d744
mikail <myay@github.com>
2017-11-25 23:25:18 +0100

    2d radix-2 fft correct results

--- FFT_2D_Radix-2/fft_2d_radix-2.c
+++ FFT_2D_Radix-2/fft_2d_radix-2.c
@@ -32,7 +32,6 @@ static const unsigned char BitReverseTable[256] =
 static const unsigned char BitReverseTable[4] = { 0x0, 0x2, 0x1, 0x3 };
 
 double complex input[X][Y];
-double complex z_temp; // for swapping complex numbers
 
 void read_input (const char* file_name)
 {
@@ -72,9 +71,9 @@ void print_array (double complex toprint[][Y])
   printf("\n");
 }
 
-void complex_swap( uint8_t x, uint8_t i, uint8_t j )
+void complex_swap_rows( uint8_t x, uint8_t i, uint8_t j )
 {
-  z_temp = input[x][i];
+  double complex z_temp = input[x][i];
   input[x][i] = input[x][j];
   input[x][j] = z_temp;
 }
@@ -90,7 +89,103 @@ void bit_reverse_rows(void)
       j = BitReverseTable[i];
       if (i < j)
       {
-        complex_swap(x, i, j);
+        complex_swap_rows(x, i, j);
+      }
+    }
+  }
+}
+
+void complex_swap_collumns( uint8_t y, uint8_t i, uint8_t j )
+{
+  double complex z_temp = input[i][y];
+  input[i][y] = input[j][y];
+  input[j][y] = z_temp;
+}
+
+void bit_reverse_collumns(void)
+{
+  //swap in place, for every collumn
+  uint8_t y, i, j;
+  for ( y = 0; y < Y; y++ )
+  {
+    for ( i = 0; i < X; i++ )
+    {
+      j = BitReverseTable[i];
+      if (i < j)
+      {
+        complex_swap_collumns(y, i, j);
+      }
+    }
+  }
+}
+
+void fft_rows(int log_x)
+{
+  double complex twiddle_base, twiddle_factor;
+  double complex top, bottom;
+  int m;
+  int stage;
+  int x;
+
+  for ( x = 0; x < X; x++ )
+  {
+    for ( stage = 1; stage <= log_x; stage++ ) // log2(N) times
+    {
+      m = pow( 2, stage );
+      double cos_arg = (2*M_PI)/m ;
+      double sin_arg = (-1)*cos_arg;
+      twiddle_base = cos( cos_arg ) + sin( sin_arg ) * I;
+
+      for ( int k = 0; k < Y; k += m )
+      {
+        twiddle_factor = 1;
+
+        for ( int j = 0; j < (m/2); j++ )
+        {
+          top = input[x][ k + j ];
+          bottom = twiddle_factor * input[x][ k + j + m/2 ];
+
+          input[x][ k + j ] = (top + bottom);
+          input[x][ k + j + m/2 ] = (top - bottom);
+
+          twiddle_factor *= twiddle_base;
+        }
+      }
+    }
+  }
+}
+
+void fft_collumns(int log_y)
+{
+  double complex twiddle_base, twiddle_factor;
+  double complex top, bottom;
+  int m;
+  int stage;
+  int y;
+
+  for ( y = 0; y < Y; y++ )
+  {
+    for ( stage = 1; stage <= log_y; stage++ ) // log2(N) times
+    {
+      m = pow( 2, stage );
+      double cos_arg = (2*M_PI)/m ;
+      double sin_arg = (-1)*cos_arg;
+      twiddle_base = cos( cos_arg ) + sin( sin_arg ) * I;
+
+      for ( int k = 0; k < X; k += m )
+      {
+        twiddle_factor = 1;
+
+        for ( int j = 0; j < (m/2); j++ )
+        {
+          top = input[ k + j ][y];
+          bottom = twiddle_factor * input[ k + j + m/2 ][y];
+
+          input[ k + j ][y] = (top + bottom);
+          input[ k + j + m/2 ][y] = (top - bottom);
+
+          twiddle_factor *= twiddle_base;
+        }
       }
     }
   }
@@ -98,6 +193,32 @@ void bit_reverse_rows(void)
 
 void fft_2d()
 {
+  bit_reverse_rows();
+
+  int divide;
+  int log_dim;
+
+  divide = X;
+  log_dim = 0;
+  while ( (divide/=2) != 0 )
+  {
+    log_dim++;
+  }
+  printf("Log2(X) = %d\n", log_dim);
+
+  fft_rows(log_dim);
+
+  bit_reverse_collumns();
+
+  divide = Y;
+  log_dim = 0;
+  while ( (divide/=2) != 0 )
+  {
+    log_dim++;
+  }
+  printf("Log2(Y) = %d\n", log_dim);
+
+  fft_collumns(log_dim);
 
 }
 
@@ -112,11 +233,6 @@ int main (int argc, char *argv[])
   read_input(argv[1]);
 
   printf("\nInput:\n");
-  print_array(input);
-
-  bit_reverse_rows();
-
-  printf("\nInput bit-reversed:\n");
   print_array(input);
 
   fft_2d();
